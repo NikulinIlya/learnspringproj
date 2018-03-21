15b6d48bff7ffb8d78d003f25d363619e00858d6
Ilya <ilia.nikulin@gmail.com>
2018-03-21 14:54:32 +0300

    Этап 3 - профилирование продолжение

--- src/main/java/quoters/ProfilingHandlerBeanPostProcessor.java
+++ src/main/java/quoters/ProfilingHandlerBeanPostProcessor.java
@@ -3,11 +3,24 @@ package quoters;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import java.lang.management.ManagementFactory;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ProfilingHandlerBeanPostProcessor implements BeanPostProcessor {
     private Map<String, Class> map = new HashMap<>();
+    private ProfilingController controller = new ProfilingController();
+
+    public ProfilingHandlerBeanPostProcessor() throws Exception {
+        MBeanServer platformMBeanServer = ManagementFactory.getPlatformMBeanServer();
+        platformMBeanServer.registerMBean(controller, new ObjectName("profiling", "name", "controller"));
+    }
+
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
         Class<?> beanClass = bean.getClass();
@@ -18,7 +31,26 @@ public class ProfilingHandlerBeanPostProcessor implements BeanPostProcessor {
     }
 
     @Override
-    public Object postProcessAfterInitialization(Object o, String s) throws BeansException {
-        return null;
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        Class beanClass = map.get(beanName);
+        if (beanClass != null) {
+            return Proxy.newProxyInstance(beanClass.getClassLoader(), beanClass.getInterfaces(), new InvocationHandler() {
+                @Override
+                public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                    if (controller.isEnabled()) {
+                        System.out.println("Профилирую...");
+                        long before = System.nanoTime();
+                        Object retVal = method.invoke(bean, args);
+                        long after = System.nanoTime();
+                        System.out.println(after - before);
+                        System.out.println("Всё");
+                        return retVal;
+                    } else {
+                        return method.invoke(bean, args);
+                    }
+                }
+            });
+        }
+        return bean;
     }
 }
